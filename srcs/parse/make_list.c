d167650856a1acc30ab775d55c02903b7ff9a95b
sanghhan <hanhee766@gmail.com>
2024-07-13 22:16:49 +0900

    따옴표 없애기 + 연결리스트 만들기 해야됨!!

--- srcs/parse/make_list.c
+++ srcs/parse/make_list.c
@@ -6,8 +6,20 @@
 /*   By: sanghhan <sanghhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 21:36:22 by sanghhan          #+#    #+#             */
-/*   Updated: 2024/07/13 17:14:04 by sanghhan         ###   ########.fr       */
+/*   Updated: 2024/07/13 22:16:22 by sanghhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
+
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (lst == 0)
+		return ;
+	if (*lst == 0)
+		*lst = new;
+	else
+		(ft_lstlast(*lst))->next = new;
+}
+
