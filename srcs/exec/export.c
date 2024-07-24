8a9cd5102f0a9a1ca43e830c6334b66249ae699c
Woosung Park <woosupar@cx2r3s7.42seoul.kr>
2024-07-24 19:36:15 +0900

    export modi

--- srcs/exec/export.c
+++ srcs/exec/export.c
@@ -6,7 +6,7 @@
 /*   By: woosupar <woosupar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 22:29:54 by woosupar          #+#    #+#             */
-/*   Updated: 2024/07/23 01:49:17 by woosupar         ###   ########.fr       */
+/*   Updated: 2024/07/24 19:35:25 by woosupar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,13 @@ int	export_builtin(t_data *data)
 	while (data->argv[i] != 0)
 	{
 		eq_index = find_equals(data->argv[i]);
+		if (eq_index == 0)
+		{	
+			ft_putstr_fd("minish: ", 2);
+			ft_putstr_fd("export: ", 2);
+			ft_putstr_fd(data->argv[i], 2);
+			ft_putstr_fd(": not a valid identifier\n", 2);
+		}
 		if (eq_index != -1)
 			do_export(data, data->argv[i], eq_index);
 		i++;
